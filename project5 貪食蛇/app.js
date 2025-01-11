a435f7903844679f89c53dce168b05976d6b130b
Eric <eric032807@gmail.com>
2025-01-11 17:50:16 -0500

    Draw Snake Final

--- project5 貪食蛇/app.js
+++ project5 貪食蛇/app.js
@@ -6,29 +6,32 @@ const ctx = canvas.getContext("2d");
 const unit = 20; //格子單位數
 const row = canvas.height / unit; //EG:320/20=16
 const column = canvas.width / unit; //EG:320/20=16
+
 //Snake
 let snake = []; //array 中的每一個元素，都是一個物件object
-//object 是儲存蛇身體的X,Y座標
-//Snake 的座標
-snake[0] = {
-  x: 80,
-  y: 0,
-};
+function createSnake() {
+  //object 是儲存蛇身體的X,Y座標
+  //Snake 的座標
+  snake[0] = {
+    x: 80,
+    y: 0,
+  };
 
-snake[1] = {
-  x: 60,
-  y: 0,
-};
+  snake[1] = {
+    x: 60,
+    y: 0,
+  };
 
-snake[2] = {
-  x: 40,
-  y: 0,
-};
+  snake[2] = {
+    x: 40,
+    y: 0,
+  };
 
-snake[3] = {
-  x: 20,
-  y: 0,
-};
+  snake[3] = {
+    x: 20,
+    y: 0,
+  };
+}
 
 //果實
 class Fruit {
@@ -36,13 +39,45 @@ class Fruit {
     this.x = Math.floor(Math.random() * column) * unit;
     this.y = Math.floor(Math.random() * row) * unit;
   }
-
+  //果實設定
   drawFruit() {
-    ctx.fillStyle = "yellow";
+    ctx.fillStyle = "lightyellow";
     ctx.fillRect(this.x, this.y, unit, unit);
+  }
+  //果實位置
+  pickALocation() {
+    let overlapping = false;
+    let new_x;
+    let new_y;
+    //果實位置不會生成在蛇身上
+    function checkOverlap(new_x, new_y) {
+      for (let i = 0; i < snake.length; i++) {
+        if (new_x == snake[i].x && new_y == snake[i].y) {
+          console.log("overlapping...");
+          overlapping = true;
+          return;
+        } else {
+          overlapping = false;
+        }
+      }
+    }
+
+    do {
+      new_x = Math.floor(Math.random() * column) * unit;
+      new_y = Math.floor(Math.random() * row) * unit;
+      console.log("果實新座標");
+      console.log(new_x, new_y);
+      checkOverlap(new_x, new_y);
+    } while (overlapping);
+
+    this.x = new_x;
+    this.y = new_y;
   }
 }
 
+//初始設定
+createSnake();
+let myFruit = new Fruit();
 //控制方向
 window.addEventListener("keydown", changeDirection);
 //初始前進方向
@@ -58,12 +93,32 @@ function changeDirection(e) {
   } else if (e.key == "ArrowUp" && d != "Down") {
     d = "Up";
   }
+  //每次控制方向，在下一次被畫出之前
+  //不接受讓和kewdown事件，不然會在程式上自殺
+  window.removeEventListener("keydown", changeDirection);
 }
+let highestScore;
+loadHighestScore();
+let score = 0;
+document.getElementById("myScore").innerHTML = "Game Score:" + score;
+document.getElementById("myScore2").innerHTML = "Highest Score:" + highestScore;
 
-//蛇,背景的顏色和呈現
+//蛇,背景,果實的顏色和呈現
 function draw() {
+  //每次畫圖，判斷蛇有沒有咬到自己
+  for (let i = 1; i < snake.length; i++) {
+    if (snake[i].x == snake[0].x && snake[i].y == snake[0].y) {
+      clearInterval(myGame);
+      alert("Game Over");
+      return;
+    }
+  }
+
   ctx.fillStyle = "black"; //設定背景全黑
   ctx.fillRect(0, 0, canvas.width, canvas.height); //畫出背景
+
+  //畫出果實
+  myFruit.drawFruit();
 
   //畫出蛇
   for (let i = 0; i < snake.length; i++) {
@@ -112,11 +167,41 @@ function draw() {
     y: snakeY,
   };
 
+  //蛇吃果實
+  if (snake[0].x == myFruit.x && snake[0].y == myFruit.y) {
+    //重新選擇和畫出果實位置
+    myFruit.pickALocation();
+    //更新分數
+    score++;
+    setHighestScore(score);
+    document.getElementById("myScore").innerHTML = "Game Score:" + score;
+    document.getElementById("myScore2").innerHTML =
+      "Highest Score:" + highestScore;
+  } else {
+    snake.pop();
+  }
   //確認蛇吃到果實，有吃會增長，沒吃會消失尾段和建立新頭。
-  snake.pop();
   snake.unshift(newHead);
+  window.addEventListener("keydown", changeDirection);
 }
 
 //蛇的移動
 //setInterval(變數名, 毫秒) 多少豪秒自行運作一次
 let myGame = setInterval(draw, 100);
+
+//載入最高分數
+function loadHighestScore() {
+  if (localStorage.getItem("highestScore") == null) {
+    highestScore = 0;
+  } else {
+    highestScore = Number(localStorage.getItem("highestScore"));
+  }
+}
+
+//判斷分數是否有最高
+function setHighestScore(score) {
+  if (score > highestScore) {
+    localStorage.setItem("highestScore", score);
+    highestScore = score;
+  }
+}
