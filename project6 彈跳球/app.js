e5e7c820cb8a658779f91ea7afe3df3aa53129bb
eric <eric032807@gmail.com>
2025-01-12 19:22:14 -0500

    Ball Game finish

--- project6 彈跳球/app.js
+++ project6 彈跳球/app.js
@@ -14,12 +14,87 @@ let ground_x = 100;
 let ground_y = 500;
 let ground_Height = 5;
 
+//Brick
+let brickArray = [];
+let count = 0;
+
+//min, max換算一個區間內的值，這樣的funcition 很實用
+function getRandomArbitrary(min, max) {
+  return min + Math.floor(Math.random() * (max - min));
+}
+
+class Brick {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.width = 100;
+    this.height = 20;
+    brickArray.push(this);
+    this.visible = true;
+  }
+
+  drawBrick() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  touchingBall(ballX, ballY) {
+    return (
+      ballX >= this.x - radius &&
+      ballX <= this.x + this.width + radius &&
+      ballY <= this.y + this.height + radius &&
+      ballY >= this.y - radius
+    );
+  }
+}
+
+//Create All Brick position
+for (let i = 0; i < 10; i++) {
+  new Brick(getRandomArbitrary(0, 900), getRandomArbitrary(10, 580));
+}
+
 //mouse 移動
 c.addEventListener("mousemove", (e) => {
   ground_x = e.clientX;
 });
 
 function drawCircle() {
+  //Ball conflict brick
+  brickArray.forEach((brick) => {
+    if (brick.visible && brick.touchingBall(circle_x, circle_y)) {
+      count++;
+      console.log(count);
+      brick.visible = false;
+      //改變x, y方向速度，remove birck
+      //從下方撞擊
+      if (circle_y >= brick.y + brick.height) {
+        ySpeed *= -1;
+      }
+      //從上方撞擊
+      else if (circle_y <= brick.y) {
+        ySpeed *= -1;
+      }
+      //從左方
+      else if (circle_x <= brick.x) {
+        xSpeed *= -1;
+      }
+      //從右方
+      else if (circle_x >= brick.x + birck.width) {
+        xSpeed *= -1;
+      }
+      //這樣的作法性能會耗費太大
+      // brickArray.splice(index, 1);
+      // if (brickArray.length == 0) {
+      //   alert("Game Over");
+      //   clearInterval(game);
+      // }
+      if (count == 10) {
+        alert("Game Over");
+        clearInterval(game);
+      }
+    }
+  });
+
   //Hit Floor
   if (
     circle_x >= ground_x - radius &&
@@ -35,6 +110,7 @@ function drawCircle() {
     }
     ySpeed *= -1;
   }
+
   //牆壁回彈
   //確認右邊邊界
   if (circle_x >= canvasWidth - radius) {
@@ -57,9 +133,16 @@ function drawCircle() {
   circle_x += xSpeed;
   circle_y += ySpeed;
 
-  // BG
+  //畫出BackGround
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+  //畫出所有Brick
+  brickArray.forEach((brick) => {
+    if (brick.visible) {
+      brick.drawBrick();
+    }
+  });
 
   // Floor
   ctx.fillStyle = "lightblue";
